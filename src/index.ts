6108732a6ba266a0c9507edeb577f2f01de76ebf
Syntek-Studio <sam.bailey@syntekstudio.com>
2025-11-24 15:28:43 +0000

    feat(start): Starting fresh due to theme issues

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Main entry point
- *
- * Default exports (web components)
- * For React Native, use: import { Button } from '@your-org/ui-design-template/native'
- */
-
-export * from './web'
