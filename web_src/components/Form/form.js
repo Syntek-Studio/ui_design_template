2cd2d0b440d0fb9d4f8648ce3cd75203b3ecfaf6
Syntek-Studio <sam.bailey@syntekstudio.com>
2025-11-24 10:16:41 +0000

    Start from scratch

--- web_src/components/Form/form.js
+++
@@ -1,6 +0,0 @@
-export { default as TextInput } from './TextInput';
-export { default as TextArea } from './TextArea';
-export { default as Select } from './Select';
-export { default as Checkbox } from './Checkbox';
-export { default as Radio } from './Radio';
-export { default as ToggleSwitch } from './ToggleSwitch';
--- web_src/components/Utility/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Loader = () => (
-  <div className="flex justify-center items-center p-4">
-    <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-logo"></div>
-  </div>
-);
-
-export default Loader;
